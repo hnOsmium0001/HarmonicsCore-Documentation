97bada835f180d2ae1764ea9e86fbb80143da9ee
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-10-21 23:21:17 -0700

    Mod setup

---
+++ src/main/java/io/github/hnosmium0001/hcdemo/DemoMod.java
@@ -0,0 +1,12 @@
+package io.github.hnosmium0001.hcdemo;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DemoMod.MODID)
+public class DemoMod {
+
+    public static final String MODID = "hcdemo";
+
+    public DemoMod() {
+    }
+}
